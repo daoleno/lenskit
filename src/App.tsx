bc1246f62ca7b46e22e9eb7f026a488c9e5f7f49
daoleno <daodaoonline@gmail.com>
2022-09-18 04:49:16 +0800

    convert to typescript project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import "./App.css";
-import logo from "./logo.svg";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="">
-          Edit <code>src/App.js</code> and save to reload.
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
