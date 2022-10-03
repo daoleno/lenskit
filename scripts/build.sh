84330c88f37936491ff9485e5cc8ae45299f4fa0
daoleno <daodaoonline@gmail.com>
2022-10-04 05:12:51 +0800

    feat(packages): reorganising the package structure with yarn workspace, add build scripts

---
+++ scripts/build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR=$(cd ${0%/*} && pwd -P)
+
+# Known variables
+SRC='./src'
+DST='./dist'
+name="lenskit"
+input="./${SRC}/index.ts"
+
+# Find executables
+esbuild=$(yarn bin esbuild)
+tsc=$(yarn bin tsc)
+resolver="${SCRIPT_DIR}/resolve-files.js"
+rewriteImports="${SCRIPT_DIR}/rewrite-imports.js"
+
+# Setup shared options for esbuild
+sharedOptions=()
+sharedOptions+=("--platform=browser")
+sharedOptions+=("--target=es2019")
+
+# Generate actual builds
+# ESM
+resolverOptions=()
+resolverOptions+=($SRC)
+resolverOptions+=('/**/*.{ts,tsx}')
+resolverOptions+=('--ignore=.test.,__mocks__')
+INPUT_FILES=$($resolver ${resolverOptions[@]})
+
+NODE_ENV=production  $esbuild $INPUT_FILES --format=esm --outdir=$DST               --outbase=$SRC --minify --pure:React.createElement ${sharedOptions[@]} &
+NODE_ENV=production  $esbuild $input       --format=esm --outfile=$DST/$name.esm.js --outbase=$SRC --minify --pure:React.createElement ${sharedOptions[@]} &
+
+# Common JS
+NODE_ENV=production  $esbuild $input --format=cjs --outfile=$DST/$name.prod.cjs --minify --bundle --pure:React.createElement ${sharedOptions[@]} $@ &
+NODE_ENV=development $esbuild $input --format=cjs --outfile=$DST/$name.dev.cjs           --bundle --pure:React.createElement ${sharedOptions[@]} $@ &
+
+# Generate types
+tsc --emitDeclarationOnly --outDir $DST &
+
+# Copy build files over
+cp -rf ./build/ $DST
+
+# Wait for all the scripts to finish
+wait
+
+# Rewrite ESM imports 😤
+$rewriteImports "$DST" '/**/*.js'
+$rewriteImports "$DST" '/**/*.d.ts'
+
+# Remove test related files
+rm -rf `$resolver "$DST" '/**/*.{test,__mocks__,}.*'`
+rm -rf `$resolver "$DST" '/**/test-utils/*'`
---
+++ scripts/watch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+# Known variables
+outdir="./dist"
+name="lenskit"
+input="./src/index.ts"
+
+# Find executables
+esbuild=$(yarn bin esbuild)
+tsc=$(yarn bin tsc)
+
+# Setup shared options for esbuild
+sharedOptions=()
+sharedOptions+=("--bundle")
+sharedOptions+=("--platform=browser")
+sharedOptions+=("--target=es2020")
+
+# Generate actual builds
+$esbuild $input --format=esm  --outfile=$outdir/$name.esm.js --sourcemap ${sharedOptions[@]} $@ --watch
+
