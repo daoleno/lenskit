6fc83ac0aac87f402c741e9877b5f6285178dce1
daoleno <daodaoonline@gmail.com>
2022-10-08 01:05:18 +0800

    feat(gql): generate react hooks and types through graphql-codegen

---
+++ codegen.ts
@@ -0,0 +1,24 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'https://api-mumbai.lens.dev',
+  documents: 'graphql',
+  generates: {
+    'packages/@lenskit-react/src/generated-gql.ts': {
+      // preset: 'client',
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+        'typed-document-node',
+        'fragment-matcher',
+      ],
+      config: {
+        dedupeFragments: true,
+      },
+    },
+  },
+}
+
+export default config
