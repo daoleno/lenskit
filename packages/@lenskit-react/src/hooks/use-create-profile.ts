85a3d9a33ef056f76a8ed4a6d6699291cdce68bf
daoleno <daodaoonline@gmail.com>
2022-10-18 06:47:10 +0800

    feat(profile): add useCreateProfile hook

---
+++ packages/@lenskit-react/src/hooks/use-create-profile.ts
@@ -0,0 +1,27 @@
+import { useCallback, useState } from 'react'
+import { createProfile as createProfileAPI } from '../lensapi/profile/create-profile'
+
+export function useCreateProfile() {
+  const [profileId, setProfileId] = useState<string | null>(null)
+  const [error, setError] = useState<Error | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const createProfile = useCallback(async (handle: string) => {
+    setIsLoading(true)
+    setError(null)
+    try {
+      const profileId = await createProfileAPI(handle)
+      setProfileId(profileId)
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error)
+      } else {
+        setError(new Error('An unknown error occurred.'))
+      }
+    } finally {
+      setIsLoading(false)
+    }
+  }, [])
+
+  return { profileId, error, isLoading, createProfile }
+}
