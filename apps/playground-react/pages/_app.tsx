9f95ab9a99238869f6919916044ca561ec71484b
daoleno <daodaoonline@gmail.com>
2022-10-30 09:13:05 +0800

    introduce tailwindcss in playgroud-react

--- apps/playground-react/pages/_app.tsx
+++ apps/playground-react/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
+import { LensKitProvider } from '@lenskit/react'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <LensKitProvider>
+      <Component {...pageProps} />
+    </LensKitProvider>
+  )
 }
