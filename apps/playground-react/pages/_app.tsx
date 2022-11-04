fbb383dc25f69f7cd6c9d26c05ce41f85f934e9d
daoleno <daodaoonline@gmail.com>
2022-11-05 07:01:47 +0800

    put apollo client inside LensKitProvider

--- apps/playground-react/pages/_app.tsx
+++ apps/playground-react/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { LensKitProvider } from '@lenskit/react'
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import '@rainbow-me/rainbowkit/styles.css'
@@ -25,15 +24,10 @@ const wagmiClient = createClient({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
-  const client = new ApolloClient({
-    uri: 'https://api-mumbai.lens.dev',
-    cache: new InMemoryCache(),
-  })
-
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
-        <LensKitProvider apolloClient={client}>
+        <LensKitProvider>
           <Component {...pageProps} />
         </LensKitProvider>
       </RainbowKitProvider>
--- packages/@lenskit-react/src/components/LensKitProvider.tsx
+++ packages/@lenskit-react/src/components/LensKitProvider.tsx
@@ -1,9 +1,67 @@
-import { ApolloClient, ApolloProvider } from '@apollo/client'
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  DefaultOptions,
+  from,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
+import { getAuthenticationToken } from 'lensapi/state'
 
 export interface LensKitProviderProps {
   apolloClient: ApolloClient<any>
   children: React.ReactNode
 }
-export function LensKitProvider({ children, apolloClient }: LensKitProviderProps) {
-  return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+}
+const httpLink = new HttpLink({
+  uri: 'https://api-mumbai.lens.dev',
+  fetch,
+})
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+    )
+
+  if (networkError) console.log(`[Network error]: ${networkError}`)
+})
+
+// example how you can pass in the x-access-token into requests using `ApolloLink`
+const authLink = new ApolloLink((operation, forward) => {
+  const token = getAuthenticationToken()
+  console.log('jwt token:', token)
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      'x-access-token': token ? `Bearer ${token}` : '',
+    },
+  })
+
+  // Call the next link in the middleware chain.
+  return forward(operation)
+})
+
+const client = new ApolloClient({
+  uri: 'https://api-mumbai.lens.dev',
+  link: from([errorLink, authLink, httpLink]),
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
+})
+
+export function LensKitProvider({ children }: LensKitProviderProps) {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>
 }
