1be2c8f837cf3dc2432b421927e24f87ea2954a6
daoleno <daodaoonline@gmail.com>
2022-11-11 02:52:11 +0800

    update ProfileCard

--- apps/playground-react/components/ProfileCard.tsx
+++ apps/playground-react/components/ProfileCard.tsx
@@ -1,10 +1,10 @@
 import { useProfiles } from '@lenskit/react'
-import { Avatar, Card, Group, Text, TextInput } from '@mantine/core'
+import { Alert, Avatar, Card, Group, Loader, Stack, Text, TextInput, Title } from '@mantine/core'
 import { useEffect, useState } from 'react'
 
 export default function ProfileCard() {
   const [profileId, setProfileId] = useState('')
-  const { profiles } = useProfiles([profileId])
+  const { profiles, loading, error } = useProfiles([profileId])
 
   useEffect(() => {
     console.log('profiles', profiles)
@@ -12,25 +12,31 @@ export default function ProfileCard() {
 
   return (
     <Card p="lg" radius="md" withBorder>
-      {profiles && (
-        <Group>
-          <Avatar src={'/lens/lens-logo.svg'} size="xl" />
-          <div style={{ flex: 1 }}>
-            <Text size="sm" weight={500}>
-              {profiles[0].name}
-            </Text>
+      <Stack>
+        <Title order={3}>Profile</Title>
+        <TextInput
+          value={profileId}
+          placeholder="Profile ID"
+          onChange={(e) => setProfileId(e.currentTarget.value)}
+        />
 
-            <Text color="dimmed" size="xs">
-              {profiles[0].bio}
-            </Text>
-          </div>
-        </Group>
-      )}
-      <TextInput
-        value={profileId}
-        placeholder="Profile ID"
-        onChange={(e) => setProfileId(e.currentTarget.value)}
-      />
+        {profiles && (
+          <Group>
+            <Avatar src={'/lens/lens-logo.svg'} size="xl" />
+            <div style={{ flex: 1 }}>
+              <Text size="sm" weight={500}>
+                {profiles[0].name}
+              </Text>
+
+              <Text color="dimmed" size="xs">
+                {profiles[0].bio}
+              </Text>
+            </div>
+          </Group>
+        )}
+        {loading && <Loader />}
+        {error && <Alert color="red">Error: {error.message}</Alert>}
+      </Stack>
     </Card>
   )
 }
