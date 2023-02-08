d3804854da537bf822220cec133197d512bcfd18
daoleno <daodaoonline@gmail.com>
2023-02-08 17:17:41 +0800

    add goatcounter

--- apps/lens-profile/pages/_app.tsx
+++ apps/lens-profile/pages/_app.tsx
@@ -2,8 +2,12 @@ import { LensKitProvider } from '@lenskit/react'
 import '@lenskit/react/styles.css'
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from 'react'
 import { WagmiConfig, chain, configureChains, createClient } from 'wagmi'
 import { publicProvider } from 'wagmi/providers/public'
+
 import './globals.css'
 // const vt333 = Public_Sans({
 //   weight: '400',
@@ -28,11 +32,37 @@ const wagmiClient = createClient({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(
+    function sendGoatCounterEventsOnRoute() {
+      const handleRouteChange = (path: string) => {
+        // @ts-ignore
+        window?.goatcounter?.count?.({
+          path,
+        })
+      }
+      router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+    },
+    [router.events]
+  )
+
   return (
-    <WagmiConfig client={wagmiClient}>
-      <LensKitProvider apiEndpoint="https://api.lens.dev">
-        <Component {...pageProps} />
-      </LensKitProvider>
-    </WagmiConfig>
+    <>
+      <Script
+        async
+        data-goatcounter="https://lens-profile.goatcounter.com/count"
+        src="//gc.zgo.at/count.js"
+        strategy="afterInteractive"
+      />
+
+      <WagmiConfig client={wagmiClient}>
+        <LensKitProvider apiEndpoint="https://api.lens.dev">
+          <Component {...pageProps} />
+        </LensKitProvider>
+      </WagmiConfig>
+    </>
   )
 }
