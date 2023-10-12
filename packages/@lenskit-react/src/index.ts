c849732a7e8e5cfb0d4e659f91be525607b58dbb
daoleno <daodaoonline@gmail.com>
2023-10-12 17:14:41 +0800

    remove lenskit-client & lenskit-react

--- packages/@lenskit-react/src/index.ts
+++
@@ -1,8 +0,0 @@
-import './tailwind.css'
-
-export * from './components/LensKitButton'
-export * from './components/LensKitProvider'
-export * from './hooks'
-export * from './types/generic'
-export * from './types/profile-metadata'
-export * from './types/publication'
