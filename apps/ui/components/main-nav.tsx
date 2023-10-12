f4564acccc6a7f14b9da77154b744934f4968c68
daoleno <daodaoonline@gmail.com>
2023-10-13 02:22:46 +0800

    feat: clone shadcn/ui and adjust layout

---
+++ apps/ui/components/main-nav.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { Icons } from '@/components/icons'
+import { siteConfig } from '@/config/site'
+import { cn } from '@/lib/utils'
+
+export function MainNav() {
+  const pathname = usePathname()
+
+  return (
+    <div className="mr-4 hidden md:flex">
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+        <Icons.logo className="h-6 w-6" />
+        <span className="hidden font-bold sm:inline-block">{siteConfig.name}</span>
+      </Link>
+      <nav className="flex items-center space-x-6 text-sm font-medium">
+        <Link
+          href="/docs"
+          className={cn(
+            'transition-colors hover:text-foreground/80',
+            pathname === '/docs' ? 'text-foreground' : 'text-foreground/60'
+          )}
+        >
+          Documentation
+        </Link>
+        <Link
+          href="/docs/components"
+          className={cn(
+            'transition-colors hover:text-foreground/80',
+            pathname?.startsWith('/docs/components') ? 'text-foreground' : 'text-foreground/60'
+          )}
+        >
+          Components
+        </Link>
+        <Link
+          href="/examples"
+          className={cn(
+            'transition-colors hover:text-foreground/80',
+            pathname?.startsWith('/examples') ? 'text-foreground' : 'text-foreground/60'
+          )}
+        >
+          Examples
+        </Link>
+        <Link
+          href={siteConfig.links.github}
+          className={cn(
+            'hidden text-foreground/60 transition-colors hover:text-foreground/80 lg:block'
+          )}
+        >
+          GitHub
+        </Link>
+      </nav>
+    </div>
+  )
+}
---
+++ apps/ui/config/docs.ts
@@ -0,0 +1,163 @@
+import { MainNavItem, SidebarNavItem } from 'types/nav'
+
+interface DocsConfig {
+  mainNav: MainNavItem[]
+  sidebarNav: SidebarNavItem[]
+}
+
+export const docsConfig: DocsConfig = {
+  mainNav: [
+    {
+      title: 'Documentation',
+      href: '/docs',
+    },
+    {
+      title: 'Components',
+      href: '/docs/components/accordion',
+    },
+    {
+      title: 'Themes',
+      href: '/themes',
+    },
+    {
+      title: 'Examples',
+      href: '/examples',
+    },
+    {
+      title: 'Figma',
+      href: '/docs/figma',
+    },
+    {
+      title: 'GitHub',
+      href: 'https://github.com/shadcn/ui',
+      external: true,
+    },
+    {
+      title: 'Twitter',
+      href: 'https://twitter.com/shadcn',
+      external: true,
+    },
+  ],
+  sidebarNav: [
+    {
+      title: 'Getting Started',
+      items: [
+        {
+          title: 'Introduction',
+          href: '/docs',
+          items: [],
+        },
+        {
+          title: 'Installation',
+          href: '/docs/installation',
+          items: [],
+        },
+        {
+          title: 'components.json',
+          href: '/docs/components-json',
+          items: [],
+        },
+        {
+          title: 'Theming',
+          href: '/docs/theming',
+          items: [],
+        },
+        {
+          title: 'Dark mode',
+          href: '/docs/dark-mode',
+          items: [],
+        },
+        {
+          title: 'CLI',
+          href: '/docs/cli',
+          items: [],
+        },
+        {
+          title: 'Typography',
+          href: '/docs/components/typography',
+          items: [],
+        },
+        {
+          title: 'Figma',
+          href: '/docs/figma',
+          items: [],
+        },
+        {
+          title: 'Changelog',
+          href: '/docs/changelog',
+          items: [],
+        },
+        {
+          title: 'About',
+          href: '/docs/about',
+          items: [],
+        },
+      ],
+    },
+    {
+      title: 'Installation',
+      items: [
+        {
+          title: 'Next.js',
+          href: '/docs/installation/next',
+          items: [],
+        },
+        {
+          title: 'Vite',
+          href: '/docs/installation/vite',
+          items: [],
+        },
+        {
+          title: 'Remix',
+          href: '/docs/installation/remix',
+          items: [],
+        },
+        {
+          title: 'Gatsby',
+          href: '/docs/installation/gatsby',
+          items: [],
+        },
+        {
+          title: 'Astro',
+          href: '/docs/installation/astro',
+          items: [],
+        },
+        {
+          title: 'Laravel',
+          href: '/docs/installation/laravel',
+          items: [],
+        },
+        {
+          title: 'Manual',
+          href: '/docs/installation/manual',
+          items: [],
+        },
+      ],
+    },
+    {
+      title: 'Dark Mode',
+      items: [
+        {
+          title: 'Next.js',
+          href: '/docs/dark-mode/next',
+          items: [],
+        },
+        {
+          title: 'Vite',
+          href: '/docs/dark-mode/vite',
+          items: [],
+        },
+      ],
+    },
+    {
+      title: 'Components',
+      items: [
+        {
+          title: 'Card',
+          href: '/docs/components/card',
+          items: [],
+        },
+      ],
+    },
+  ],
+}
